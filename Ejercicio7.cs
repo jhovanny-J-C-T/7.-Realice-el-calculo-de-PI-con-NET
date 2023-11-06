981e0442cd83537bd006ff5a850353156d706088
jhovanny-J-C-T <56840398+jhovanny-J-C-T@users.noreply.github.com>
2023-11-06 04:46:35 -0400

    Add files via upload

---
+++ Ejercicio7.cs
@@ -0,0 +1,25 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.7.34221.43
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Ejercicio7", "Ejercicio7\Ejercicio7.csproj", "{8C308584-B8F9-48D6-BD6E-9E70C4360D00}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{8C308584-B8F9-48D6-BD6E-9E70C4360D00}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{8C308584-B8F9-48D6-BD6E-9E70C4360D00}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{8C308584-B8F9-48D6-BD6E-9E70C4360D00}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{8C308584-B8F9-48D6-BD6E-9E70C4360D00}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {073F6434-EC3F-4B8F-81C3-4321D79A28C7}
+	EndGlobalSection
+EndGlobal
